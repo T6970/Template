829967ec2639f2e0edff7663caedbc6a60742515
T6970 <tenaldkyf315@gmail.com>
2025-11-27 21:40:35 +0800

    Remake script.js to just load other modules

--- js/script.js
+++ js/script.js
@@ -1,25 +1,2 @@
-const editor = document.getElementById('editor')
-import "./mainApi.js"
-
-const extensions = []
-const main = async () => {
-  try {
-    const load = await fetch('../extlist.txt')
-    const extensionNames = (await load.text()).split('\n').filter(name => name.trim() !== '')
-    
-    for (let i = 0; i < extensionNames.length; i++) {
-      const extension = new Worker(`../extensions/${extensionNames[i]}.js`)
-
-      extensions.push(extension)
-    }
-
-    editor.addEventListener('keydown', (e) => {
-      extensions.forEach(extension => {
-        extension.postMessage({type: 'keypress', key: e.key})
-      })
-    })
-  } catch (e) {
-    console.error(e)
-  }
-}
-main()
+// This file just loads other modules
+import "api.js"
